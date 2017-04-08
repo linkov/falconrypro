0be4ba16ffc14b1c10079249228fe5294908f39b
Alex Linkov <alexey.linkov@gmail.com>
2017-04-08 13:51:00 +0200

    Initial commit;

---
+++ falconrypro/falconrypro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  falconrypro
+//
+//  Created by Alex Linkov on 4/8/17.
+//  Copyright © 2017 SDWR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ falconrypro/falconryproTests/falconryproTests.swift
@@ -0,0 +1,36 @@
+//
+//  falconryproTests.swift
+//  falconryproTests
+//
+//  Created by Alex Linkov on 4/8/17.
+//  Copyright © 2017 SDWR. All rights reserved.
+//
+
+import XCTest
+@testable import falconrypro
+
+class falconryproTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
